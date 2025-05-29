9d4c1e8fe3ea1edb82e0892950fcbb2b2f4898a8
matanlevi95 <matanlevi95@gmail.com>
2025-05-29 10:31:16 +0300

    Added some bootstrap style for navbar

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,8 +1,37 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-4">
+    <router-link class="navbar-brand" to="/">Vue App</router-link>
+
+    <div class="collapse navbar-collapse">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <router-link class="nav-link" to="/home">Home</router-link>
+        </li>
+        <li class="nav-item" v-if="isEditor">
+          <router-link class="nav-link" to="/editor">Editor</router-link>
+        </li>
+        <li class="nav-item" v-if="isAdmin">
+          <router-link class="nav-link" to="/admin">Admin</router-link>
+        </li>
+      </ul>
+
+      <div class="d-flex align-items-center gap-3" v-if="username">
+        <span class="text-white">{{ username }}</span>
+        <button class="btn btn-outline-light btn-sm" @click="handleLogout">
+          Logout
+        </button>
+      </div>
+    </div>
+  </nav>
+</template>
+
 <script setup>
 import { useStore } from "vuex";
+import { useRouter } from "vue-router";
 import { computed, onMounted } from "vue";
 
 const store = useStore();
+const router = useRouter();
 
 onMounted(() => {
   store.dispatch("initialize");
@@ -11,29 +40,9 @@ onMounted(() => {
 const isAdmin = computed(() => store.getters.isAdmin);
 const isEditor = computed(() => store.getters.isEditor);
 const username = computed(() => store.getters.username);
+
+const handleLogout = () => {
+  store.dispatch("logout");
+  router.push("/");
+};
 </script>
-
-<template>
-  <nav class="navbar">
-    <router-link to="/" class="hover:text-gray-300">Login</router-link>
-    <router-link to="/home" class="hover:text-gray-300">Home</router-link>
-    <router-link v-if="isAdmin" to="/admin" class="hover:text-gray-300"
-      >Admin</router-link
-    >
-    <router-link v-if="isEditor" to="/editor" class="hover:text-gray-300"
-      >Editor</router-link
-    >
-    <span v-if="username">| {{ username }}</span>
-  </nav>
-</template>
-
-<style scoped>
-.navbar {
-  display: flex;
-  justify-content: center;
-  gap: 1rem;
-  width: fit-content;
-  margin: 0 auto;
-  align-items: center;
-}
-</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,9 @@ import "./style.css"
 import App from "./App.vue"
 import router from "./router"
 import store from "./store"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap' 
+
 
 const app = createApp(App)
 
